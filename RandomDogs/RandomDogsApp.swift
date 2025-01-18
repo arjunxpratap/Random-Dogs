14dc0f36a7bd0ef837a399473269813970875027
arjunpratap17 <arjun.choudhary@rabbitinvest.com>
2025-01-18 20:57:32 +0530

    Initial Commit

---
+++ RandomDogs/RandomDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDogsApp.swift
+//  RandomDogs
+//
+//  Created by Arjun Pratap Choudhary on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomDogsTests/RandomDogsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDogsTests.swift
+//  RandomDogsTests
+//
+//  Created by Arjun Pratap Choudhary on 18/01/25.
+//
+
+import Testing
+@testable import RandomDogs
+
+struct RandomDogsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
